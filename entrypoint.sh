658560dd06dd56f7862d475d7edaf41a74e694ae
Egor Kulev <thewgosplayer@gmail.com>
2020-07-07 22:36:31 +0300

    Edit startup params

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ if [ ! -z ${SRCDS_APPID} ]; then
 fi
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo ${STARTUP} +hostname $(printf %q "\"${SRCDS_HOSTNAME}\"") | sed -e 's/{{/${/g' -e 's/}}/}/g')
+eval echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
